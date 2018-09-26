126cdb2c5e47f823e3b4c6e9c0e6f91d1e5923c4
张昌昌 <zhcc@zving.com>
2018-09-26 19:43:42 +0800

    更改功能

--- src/main/java/com/mj/controller/SpiderController.java
+++ src/main/java/com/mj/controller/SpiderController.java
@@ -1,20 +1,24 @@
 package com.mj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mj.spider.pageprocesser.MJPageProcesser;
 
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.scheduler.RedisPriorityScheduler;
 
 @RestController
 public class SpiderController {
 
+	@Autowired
+	private RedisPriorityScheduler redisPriorityScheduler;
 
 
 	@GetMapping
 	public String test() {
-		Spider.create(new MJPageProcesser())
+		Spider.create(new MJPageProcesser()).setScheduler(redisPriorityScheduler)
 				.addUrl("http://www.peoplerail.com/rmtd2016/content/2018-09/20/node_2.htm").thread(3).run();
 		return "";
 	}
